0bc39553fe9ada667a0e9feb26364051ee78a6d5
vibecastor <vibecastor@gmail.com>
2018-04-18 15:25:15 -0700

    notes on reverse an array

--- reverse-an-array/src/reverse-an-array.js
+++ reverse-an-array/src/reverse-an-array.js
@@ -1,13 +1,29 @@
 'use strict';
 
-const array = [1, 2, 3];
+// const array = [1, 2, 3];
 
-function reverseArray(arr) {
-  const revArray = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    revArray.push(arr[i]);
+// function reverseArray(arr) {
+//   const revArray = [];
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     revArray.push(arr[i]);
+//   }
+//   return (revArray);
+// }
+
+// reverseArray(array);
+
+const reverse = (arr) => {
+  const len = arr.length;
+  const mid = Math.floor(len / 2);
+  let temp;
+
+  for (let i = 0; i < mid; i++) {
+    temp = arr[i];
+    arr[i] = arr[len - 1 - i]; // swapping?
+    arr[len - 1 - i] = temp;
   }
-  return (revArray);
-}
+  return arr;
+};
 
-reverseArray(array);
+const test = [1, 2, 3, 4, 5];
+console.log(reverse(test));
