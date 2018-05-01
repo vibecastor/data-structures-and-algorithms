a1941f09fe9ebe14c55906131d2680d147402a4d
vibecastor <vibecastor@gmail.com>
2018-05-01 07:58:59 -0700

    whiteboard11

---
+++ challenges/ll-merge/src/__test__/node.test.js
@@ -0,0 +1,11 @@
+// 'use strict';
+
+// const Node = require('../lib/node');
+
+// describe('node.js', () => {
+//   test('constructor', () => {
+//     const node = new Node('Bear Dog!');
+//     expect(node.value).toEqual('Bear Dog!');
+//     expect(node.next).toEqual(null);
+//   });
+// });
