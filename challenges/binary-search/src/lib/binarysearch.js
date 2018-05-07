789a5a05867f7ed2d323b5c3d824a1c7816b0c81
vibecastor <vibecastor@gmail.com>
2018-05-07 15:02:04 -0700

    multi-bracket

--- challenges/binary-search/src/lib/binarysearch.js
+++ challenges/binary-search/src/lib/binarysearch.js
@@ -1,17 +1,11 @@
 'use strict';
 
-const binarysearch = require('binarysearch') = {};
-
-const testArray = [1, 2, 3, 4, 5];
-const testKey = 4;
-
-binarysearch.binarySearch = (someArray, key) => {
+export default (someArray, key) => {
   for (let i = 0; i < someArray.length; i++) {
     if (someArray[i] === key) {
       return i;
     }
   }
-  return -1   ;
+  return -1;
 };
 
-binarySearch(testArray, testKey);
